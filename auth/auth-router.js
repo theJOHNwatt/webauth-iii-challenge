53568fb3b84ea8e36a6029fece2c49e7a59d2610
theJOHNwatt <j.watt10@gmail.com>
2019-11-14 12:30:52 -0800

    auth router end points created, restricted middleware created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,32 @@
-const router = require("express").Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken"); 
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 
 const Users = require('../users/users-model.js');
+const { validateUser } = require('../users/users-helpers.js');
+
+router.post('/register', (req, res) => {
+
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+function getJwtToken(username) {
+    const payload = {
+        username,
+        role
+    };
+
+    const secret = process.env.JWT_SECRET || 'supersupersecret';
+
+    const options ={
+        expiresIn: '1d'
+    };
+    
+    return jwt.sign(paylod, secret, options);
+}
 
 
 module.exports = router;
