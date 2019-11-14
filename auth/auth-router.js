cda10f745c718e4dabdecb990137747258d950da
theJOHNwatt <j.watt10@gmail.com>
2019-11-14 12:21:16 -0800

    changed a file name

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,4 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken"); 
 
+const Users = require('../users/users-model.js');
+
+
 module.exports = router;
